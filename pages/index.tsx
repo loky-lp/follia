3dc69269c6efdf7bafc4d01e634b881b4bb3b601
Lorenzo Pecchio <71527839+LoKy-dev@users.noreply.github.com>
2022-05-05 00:56:05 +0200

    Fix escaping of double and single quotes on index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,32 +13,32 @@ const Home: NextPage = () => {
 
 	    <p>Cosa vuoi fare ora?</p>
 	    <p>Che domanda stupida, so cosa voglio fare ora.</p>
-	    <p>Immergermi nella foresta sotto montagne di chiome che oscillano da una parte e dall'altra, con il sole che penetra tra le intermittenze delle foglie e stare disteso lì, guardando il cielo. Poi, nuotare nel cielo di nuvole e sentirmi così leggero da scavare in ogni piccola particella, entrando nelle profondità del più grande ballo del mondo, un ballo di idee folli, irreali, assolutamente prive di qualunque senso.</p>
+	    <p>Immergermi nella foresta sotto montagne di chiome che oscillano da una parte e dall&apos;altra, con il sole che penetra tra le intermittenze delle foglie e stare disteso lì, guardando il cielo. Poi, nuotare nel cielo di nuvole e sentirmi così leggero da scavare in ogni piccola particella, entrando nelle profondità del più grande ballo del mondo, un ballo di idee folli, irreali, assolutamente prive di qualunque senso.</p>
 	    <p>Penso questo forse perché sono in realtà una persona piuttosto semplice, ma che cerca disperatamente di entrare nei più grandi cervelloni della storia. Chissà perché sono così folle da farmi del male da solo.</p>
-	    <p>Esatto, io non ho le energie ne le competenze, neanche l'astuzia o un buon chiacchiericcio che mi permetta solo lontanamente di aspirare a diventare quelle persone che ora tengono in mano il pianeta. Che poi cosa tengono in mano se non una scatola di rottami, tra cui io.</p>
-	    <p>Nella mia vita sono bravo soltanto a confondere le idee degli altri. Insomma, seguitemi. Il cielo e il mare sono in realtà la stessa cosa, si uniscono per creare l'orizzonte e dividono di blu sia il sotto e sia il sopra. Io sono la nuvola, la schiuma, un onda tanto alta da scuotere il mare. Mi insegnano a stare fermo, ed io zitto annuisco.</p>
+	    <p>Esatto, io non ho le energie ne le competenze, neanche l&apos;astuzia o un buon chiacchiericcio che mi permetta solo lontanamente di aspirare a diventare quelle persone che ora tengono in mano il pianeta. Che poi cosa tengono in mano se non una scatola di rottami, tra cui io.</p>
+	    <p>Nella mia vita sono bravo soltanto a confondere le idee degli altri. Insomma, seguitemi. Il cielo e il mare sono in realtà la stessa cosa, si uniscono per creare l&apos;orizzonte e dividono di blu sia il sotto e sia il sopra. Io sono la nuvola, la schiuma, un onda tanto alta da scuotere il mare. Mi insegnano a stare fermo, ed io zitto annuisco.</p>
 	    <p>Perché è così che la gente ragiona, ed io annuisco.</p>
 	    <p>Che stupido sono stato. Una volta non ero così.</p>
 	    <p>Infanzia e adolescenza, mondi puri di follia. Ci entravo dentro e tutto non aveva senso. Quella però era la mia scatola, ed i rottami danzavano.</p>
 	    <p>La verità è che sto generalizzando quando le cose sono semplici... Ma come ci siamo arrivati a questa complessità? E da quando per vivere la vita decentemente bisogna essere complessi? Il problema è che questa merda la cerco. Forse in tutta questa articolata ragnatela che rappresenta le nostre aspirazioni ci siamo dimenticati chi siamo realmente, e queste strutture sociali cambiano noi stessi più di quanto la nostra volontà riesca a reagire. Il problema è che ci sentiamo schiavi di qualcosa che non ci appartiene, e questo meccanismo stupido raccoglie questi rottami nella scatola... Ma poi sto rottami... Non saremo mica noi?</p>
 	    <p>Umani rotti e rottami umani. Non so più dove sbattere la testa.</p>
-	    <p>Pazzo: "Quante stupidaggini... Ma alla fine te che vuoi fare?"</p>
-	    <p>Io: "Che stupida domanda, voglio essere me stesso."</p>
-	    <p>Pazzo: "E perché non vuoi essere me?"</p>
-	    <p>Io: "Perché te sei te ed io sono io!"</p>
-	    <p>Pazzo: "E tu credi che alle persone importi qualcosa di quello che sei?"</p>
-	    <p>La verità è che sto cercando qualcosa che possa aiutarmi a esprimere me stesso, ma non esiste strumento che lo faccia. Ne ho provati tantissimi, e me ne sono circondato, ma nessuno di questi funziona. Sai qual'è l'unico che funziona? Il torturarmi la testa fino a pensare qualcosa di folle e confuso.</p>
+	    <p>Pazzo: &ldquot;Quante stupidaggini... Ma alla fine te che vuoi fare?&rdquot;</p>
+	    <p>Io: &ldquot;Che stupida domanda, voglio essere me stesso.&rdquot;</p>
+	    <p>Pazzo: &ldquot;E perché non vuoi essere me?&rdquot;</p>
+	    <p>Io: &ldquot;Perché te sei te ed io sono io!&rdquot;</p>
+	    <p>Pazzo: &ldquot;E tu credi che alle persone importi qualcosa di quello che sei?&rdquot;</p>
+	    <p>La verità è che sto cercando qualcosa che possa aiutarmi a esprimere me stesso, ma non esiste strumento che lo faccia. Ne ho provati tantissimi, e me ne sono circondato, ma nessuno di questi funziona. Sai qual&apos;è l&apos;unico che funziona? Il torturarmi la testa fino a pensare qualcosa di folle e confuso.</p>
 	    <p>Questa è una bella scommessa doc, se la smetto, dovrò continuare ad essere mediocre nella vita, senza sedermi con i grandi...</p>
 	    <p>Quindi per essere grande dovrei essere confuso? Al diavolo allora.</p>
 	    <p>Vorrei accarezzare la pelle di qualcuno.</p>
 	    <p>Vorrei piangere.</p>
-	    <p>Il ricordo di una cena con i miei all'età di 6 anni.</p>
+	    <p>Il ricordo di una cena con i miei all&apos;età di 6 anni.</p>
 	    <p>A 7 un amico stronzo.</p>
 	    <p>A 8 una serie di amicizie sincere.</p>
 	    <p>La sfera di natale polverizza la neve in sbuffi luminosi che si diramano nel vento pallido.</p>
 	    <p>Una linea luminosa traccia il sentiero fino ad un castello di abissi scarlatti.</p>
 	    <p>Polifemo cresce e si divora ragazzi e ragazze nude. In questo vuoto, i corpi femminili attirano quelli maschili, fluttuando e accucciando i corpi in posizione fetale.</p>
-	    <p>Un albero squarcia un pianeta con le radici, e seguendole, il cuore della terra è comandato da mille fuochi d'artificio.</p>
+	    <p>Un albero squarcia un pianeta con le radici, e seguendole, il cuore della terra è comandato da mille fuochi d&apos;artificio.</p>
 	    <p>Eccomi qui.</p>
 	    <p>Sono bravo a creare scene confuse nella mia testa. Sono bravo a confondere.</p>
 	    <p className='mb-0'>Così bravo che confondo me stesso.</p>
