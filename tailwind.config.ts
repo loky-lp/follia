94f5cf861b6288dde9232e3e896ac56fb4185e83
Lorenzo Pecchio <noreply.83062271+loky-lp@lorenzopecchio.com>
2024-11-06 11:33:07 +0100

    chore: override typography margins for `h1` and `p` elements

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,34 @@ export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {},
+		extend: {
+			typography: {
+				DEFAULT: {
+					css: {
+						h1: {
+							marginTop: '1.125rem',
+							marginBottom: '1.125rem',
+						},
+						p: {
+							marginTop: '1.125rem',
+							marginBottom: '1.125rem',
+						},
+					},
+				},
+				lg: {
+					css: {
+						h1: {
+							marginTop: '1.25rem',
+							marginBottom: '1.25rem',
+						},
+						p: {
+							marginTop: '1.25rem',
+							marginBottom: '1.25rem',
+						},
+					},
+				},
+			},
+		},
 	},
 
 	plugins: [typography],
