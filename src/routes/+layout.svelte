6f30a59b0c719ef39df957a51073c9c944774564
Lorenzo Pecchio <noreply.83062271+loky-lp@lorenzopecchio.com>
2026-01-03 13:34:08 +0100

    feat: add dark mode

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
+	import { ModeWatcher } from 'mode-watcher'
+
 	import './layout.css'
 	import '@fontsource-variable/quicksand'
 
 	let { children } = $props()
 </script>
 
+<ModeWatcher />
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,7 @@
 	<title>E tu quanto sei folle?</title>
 </svelte:head>
 
-<article class={`prose lg:prose-lg ${size} p-4`}>
+<article class={`prose prose-theme lg:prose-lg ${size} p-4`}>
 	<h1>Follia</h1>
 
 	<p>Cosa vuoi fare ora?</p>
@@ -88,7 +88,7 @@
 	<p>Così bravo che confondo me stesso.</p>
 </article>
 
-<div class="border-t border-gray-200 bg-neutral-100">
+<div class="border-t bg-neutral-100 dark:bg-neutral-900">
 	<div class={`${size} text-muted px-4 py-1 text-center text-xs`}>
 		Copyright © {new Date().toISOString().split('-')[0]} - All right reserved.
 	</div>
